c9a3e286c05f9b7de876e5c8a87f12aeccbee007
Deeptanshu Malik <DeeptanshuM@users.noreply.github.com>
2017-03-25 00:49:52 -0400

    Added firebase to podfile. Let the hacking begin :)

---
+++ Podfile
@@ -0,0 +1,13 @@
+use_frameworks!
+platform :ios, '10.0'
+
+pod 'Firebase/AdMob'
+pod 'Firebase/Auth'
+pod 'Firebase/Crash'
+pod 'Firebase/Database'
+pod 'Firebase/Invites'
+pod 'Firebase/RemoteConfig'
+pod 'Firebase/Storage'
+
+target 'MHacks9' do
+end
---
+++ Pods/Target Support Files/GoogleToolboxForMac/GoogleToolboxForMac-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "GTMTypeCasting.h"
+#import "GTMLocalizedString.h"
+#import "GTMLogger.h"
+#import "GTMDebugSelectorValidation.h"
+#import "GTMDebugThreadValidation.h"
+#import "GTMMethodCheck.h"
+#import "GTMDefines.h"
+#import "GTMLogger.h"
+#import "GTMNSData+zlib.h"
+#import "GTMNSDictionary+URLArguments.h"
+#import "GTMNSString+URLArguments.h"
+#import "GTMStringEncoding.h"
+#import "GTMURLBuilder.h"
+
+FOUNDATION_EXPORT double GoogleToolboxForMacVersionNumber;
+FOUNDATION_EXPORT const unsigned char GoogleToolboxForMacVersionString[];
+
