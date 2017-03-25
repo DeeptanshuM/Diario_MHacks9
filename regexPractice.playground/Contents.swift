d66cb0056375cdecc948e1f33e86ab3b3270f6c4
Deeptanshu Malik <DeeptanshuM@users.noreply.github.com>
2017-03-25 17:00:05 -0400

    Coded and tested regexes for day date month, numberical date, only day

--- regexPractice.playground/Contents.swift
+++ regexPractice.playground/Contents.swift
@@ -7,10 +7,10 @@ print(str)
 
 ///////////DayDateMonth///////////
 
-var date = "jkhfdsjhkl dsfjlhjdfhlkjhkldsf jkhdsjkhfds Wednesday 7, April"
+var date = "jkhfdsjhkl dsfjlhjdfhlkjhkldsf jkhdsjkhfds Wednesday 7th, April"
 
 //var regex_DayDateMonth = "wednesday \\d+, \\w+"
-var regex_DayDateMonth = "(\\w+,?) (\\d+,?) (\\w+)"
+var regex_DayDateMonth = "(\\w+,?) (\\d+(st|nd|th)?,?) (\\w+)"
 
 //day date, month
 if let range = date.range(of:regex_DayDateMonth, options: [.regularExpression, .caseInsensitive]) {
@@ -38,4 +38,30 @@ if let range = date4.range(of:regex_DayDateMonth, options: [.regularExpression, 
   let result = date4.substring(with:range)
   print(result)
 }
+
 ///////////////////////////////////
+
+///////////Date (like 01.01.17)///////////
+var regex_Date = "(\\d\\d?(.|-|/)\\d\\d?(.|-|/)\\d\\d?)"
+var date5 = "jkdjkhdjkfhajkhl 01.10.98"
+if let range = date5.range(of:regex_Date, options: [.regularExpression, .caseInsensitive]) {
+  let result = date5.substring(with:range)
+  print(result)
+}
+
+var date6 = "jkdjkhdjkfhajkhl 01-10/98"
+if let range = date6.range(of:regex_Date, options: [.regularExpression, .caseInsensitive]) {
+  let result = date6.substring(with:range)
+  print(result)
+}
+///////////////////////////////////
+
+///////////Days only///////////
+var regex_days = "Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday|Mon|Tue|Wed|Thur|Thu|Fri|Sat|Sun"
+
+var date7 = "what is the string only has a day, for eg, this sunday, what then"
+if let range = date7.range(of:regex_days, options: [.regularExpression, .caseInsensitive]) {
+  let result = date7.substring(with:range)
+  print(result)
+}
+////////////////////////////////////////
