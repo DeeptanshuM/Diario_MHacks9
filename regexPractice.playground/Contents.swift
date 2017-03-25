9cc173fe1962c579482675f419bd007b05cb66a5
Deeptanshu Malik <DeeptanshuM@users.noreply.github.com>
2017-03-25 18:55:08 -0400

    about to take a different approach to regex prob

--- regexPractice.playground/Contents.swift
+++ regexPractice.playground/Contents.swift
@@ -2,8 +2,15 @@
 
 import UIKit
 
-var str = "Hello, playground"
+let dateFormatter = DateFormatter()
+dateFormatter.dateStyle = .medium
+dateFormatter.timeStyle = .none
+
+let datenow = Date(timeIntervalSinceReferenceDate: 118800)
+
+var str = ""
 print(str)
+
 
 ///////////DayDateMonth///////////
 
@@ -11,7 +18,6 @@ var date = "jkhfdsjhkl dsfjlhjdfhlkjhkldsf jkhdsjkhfds Wednesday 7th, April"
 
 //var regex_DayDateMonth = "wednesday \\d+, \\w+"
 var regex_DayDateMonth = "(\\w+,?) (\\d+(st|nd|th)?,?) (\\w+)"
-
 //day date, month
 if let range = date.range(of:regex_DayDateMonth, options: [.regularExpression, .caseInsensitive]) {
   let result = date.substring(with:range)
@@ -23,6 +29,7 @@ var date2 = "jkhfdsjhkl dsfjlhjdfhlkjhkldsf Wednesday 7, April jkhdsjkhfds "
 if let range = date2.range(of:regex_DayDateMonth, options: [.regularExpression, .caseInsensitive]) {
   let result = date2.substring(with:range)
   print(result)
+  
 }
 
 //day date month
@@ -43,10 +50,11 @@ if let range = date4.range(of:regex_DayDateMonth, options: [.regularExpression, 
 
 ///////////Date (like 01.01.17)///////////
 var regex_Date = "(\\d\\d?(.|-|/)\\d\\d?(.|-|/)\\d\\d?)"
-var date5 = "jkdjkhdjkfhajkhl 01.10.98"
+var date5 = "jkdjkhdjkfhajkhl 01.10.18"
 if let range = date5.range(of:regex_Date, options: [.regularExpression, .caseInsensitive]) {
   let result = date5.substring(with:range)
   print(result)
+  str = result
 }
 
 var date6 = "jkdjkhdjkfhajkhl 01-10/98"
@@ -57,11 +65,89 @@ if let range = date6.range(of:regex_Date, options: [.regularExpression, .caseIns
 ///////////////////////////////////
 
 ///////////Days only///////////
-var regex_days = "Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday|Mon|Tue|Wed|Thur|Thu|Fri|Sat|Sun"
+
+
+var regex_months = "jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec"
 
 var date7 = "what is the string only has a day, for eg, this sunday, what then"
-if let range = date7.range(of:regex_days, options: [.regularExpression, .caseInsensitive]) {
+if let range = date7.range(of:regex_Date, options: [.regularExpression, .caseInsensitive]) {
   let result = date7.substring(with:range)
   print(result)
 }
 ////////////////////////////////////////
+
+func getDay() -> String{
+  var regex_days = "mon|tue|wed|thu|fri|sat|sun"
+  
+  do {
+    let findday = try NSRegularExpression(pattern: regex_days, options: .caseInsensitive)
+
+ // let findday = NSRegularExpression(pattern: regex_days, options: .caseInsensitive)
+  var myStr = "Wednesday, 7th APril"
+    let matches = findday.matches(in: myStr, options: [], range: NSRange(location: 0, length: myStr.characters.count))
+    
+//    if let match = matches.first {
+//     let range = match.rangeAt(1)
+//    if let swiftRange = range.toRange(for: myStr) {
+//        let name = myStr.substring(with: swiftRange)
+//      }
+//    }
+    
+    switch "jkdsjk" {
+      case "mon":
+       return "Monday"
+    case "tue":
+      return "Tuesday"
+    case "wed":
+      return "Wednesday"
+    case "thu":
+      return "Thursday"
+    case "fri":
+      return "Friday"
+    case "sat":
+      return "Saturday"
+    case "sun":
+      return "Sunday"
+    default:
+      return ""
+    }
+  } catch let error {
+    print(error)
+  }
+  return ""
+}
+
+///////////////////
+extension String {
+  public func match(regex: String) -> String? {
+    let expression: NSRegularExpression
+    
+    //if let exists = expression[regex]
+    //{
+    //  expression = exists
+    //}
+    //else {
+    expression = try! NSRegularExpression(pattern: "^\(regex)", options: .caseInsensitive)
+    // expression[regex] = expression
+    //}
+    
+    let range = expression.rangeOfFirstMatch(in: self, options: [], range: NSMakeRange(0, utf16.count))
+    if range.location != NSNotFound {
+      return (self as NSString).substring(with: range)
+    }
+    return nil
+  }
+}
+
+let myStr = "jdfsjkdsfjks Wednesday"
+var deliverance = myStr.match(regex: "Wed")
+print(deliverance)
+
+
+//func getdd() -> String{
+//  var regex_dd = "\\d\\d?"
+ // if let findday = NSRegularExpression(pattern: regex_dd, options: .caseInsensitive){
+//    let match = findday.matchesInString(, options: .allZeros, range: range) as! [NSTextCheckingResult]
+//
+//    return match
+//}
