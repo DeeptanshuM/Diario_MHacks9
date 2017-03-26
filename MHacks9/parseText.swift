affd3a03f192a5da3fa8ba05ebdb0c84e4b39728
Deeptanshu Malik <DeeptanshuM@users.noreply.github.com>
2017-03-26 01:10:11 -0400

    created a singleton for parseText

--- MHacks9/parseText.swift
+++ MHacks9/parseText.swift
@@ -12,8 +12,9 @@ class parseText: NSObject {
   
   //example of Date it returns: Dec 21, 2017, 12:00 AM"
   //if date not found then returns nil
+  static let sharedInstance = parseText()
   
-  public class func getDate(input: String) -> Date?
+  public func getDate(input: String) -> Date?
   {
     let regex_DayDateMonth = "(\\w+,?) (\\d+(st|nd|th)?,?) (\\w+)(,|.)?"
     let regex_Date_MM_dd_yy = "(\\d\\d?(.|-|/)\\d\\d?(.|-|/)\\d\\d?)"
@@ -103,81 +104,82 @@ class parseText: NSObject {
     
     return nil
   }
-}
-
-//MARK: Private functions
-private func getDay_DayDateYear(input: String) -> String{
-  if input.range(of:"mon", options: [.regularExpression, .caseInsensitive]) != nil {
-    return "Monday"
+  
+  
+  //MARK: Private functions
+  private func getDay_DayDateYear(input: String) -> String{
+    if input.range(of:"mon", options: [.regularExpression, .caseInsensitive]) != nil {
+      return "Monday"
+    }
+    else if input.range(of:"tue", options: [.regularExpression, .caseInsensitive]) != nil {
+      return "Tuesday"
+    }
+    else if input.range(of:"wed", options: [.regularExpression, .caseInsensitive]) != nil {
+      return "Wednesday"
+    }
+    else if input.range(of:"thu", options: [.regularExpression, .caseInsensitive]) != nil {
+      return "Thursday"
+    }
+    else if input.range(of:"fri", options: [.regularExpression, .caseInsensitive]) != nil  {
+      return "Friday"
+    }
+    else if input.range(of:"sat", options: [.regularExpression, .caseInsensitive]) != nil  {
+      return "Saturday"
+    }
+    else if input.range(of:"sun", options: [.regularExpression, .caseInsensitive]) != nil  {
+      return "Sunday"
+    }
+    else{
+      return ""
+    }
   }
-  else if input.range(of:"tue", options: [.regularExpression, .caseInsensitive]) != nil {
-    return "Tuesday"
-  }
-  else if input.range(of:"wed", options: [.regularExpression, .caseInsensitive]) != nil {
-    return "Wednesday"
-  }
-  else if input.range(of:"thu", options: [.regularExpression, .caseInsensitive]) != nil {
-    return "Thursday"
-  }
-  else if input.range(of:"fri", options: [.regularExpression, .caseInsensitive]) != nil  {
-    return "Friday"
-  }
-  else if input.range(of:"sat", options: [.regularExpression, .caseInsensitive]) != nil  {
-    return "Saturday"
-  }
-  else if input.range(of:"sun", options: [.regularExpression, .caseInsensitive]) != nil  {
-    return "Sunday"
-  }
-  else{
+  
+  private func getDate_DayDateYear(input: String) -> String{
+    if let range = input.range(of:"\\d\\d?", options: [.regularExpression, .caseInsensitive]) {
+      return input.substring(with:range)
+    }
     return ""
   }
-}
-
-private func getDate_DayDateYear(input: String) -> String{
-  if let range = input.range(of:"\\d\\d?", options: [.regularExpression, .caseInsensitive]) {
-    return input.substring(with:range)
-  }
-  return ""
-}
-
-private func getMonth_DayDateYear(input: String) -> String{
-  if input.range(of:"jan", options: [.regularExpression, .caseInsensitive]) != nil {
-    return "January"
-  }
-  else if input.range(of:"feb", options: [.regularExpression, .caseInsensitive]) != nil {
-    return "February"
-  }
-  else if input.range(of:"mar", options: [.regularExpression, .caseInsensitive]) != nil {
-    return "March"
-  }
-  else if input.range(of:"apr", options: [.regularExpression, .caseInsensitive]) != nil {
-    return "April"
-  }
-  else if input.range(of:"may", options: [.regularExpression, .caseInsensitive]) != nil {
-    return "May"
-  }
-  else if input.range(of:"jun", options: [.regularExpression, .caseInsensitive]) != nil  {
-    return "June"
-  }
-  else if input.range(of:"jul", options: [.regularExpression, .caseInsensitive]) != nil  {
-    return "July"
-  }
-  else if input.range(of:"aug", options: [.regularExpression, .caseInsensitive]) != nil  {
-    return "Asgust"
-  }
-  else if input.range(of:"sep", options: [.regularExpression, .caseInsensitive]) != nil {
-    return "September"
-  }
-  else if input.range(of:"oct", options: [.regularExpression, .caseInsensitive]) != nil {
-    return "October"
-  }
-  else if input.range(of:"nov", options: [.regularExpression, .caseInsensitive]) != nil {
-    return "November"
-  }
-  else if input.range(of:"dec", options: [.regularExpression, .caseInsensitive]) != nil  {
-    return "December"
-  }
-  else{
-    return ""
+  
+  private func getMonth_DayDateYear(input: String) -> String{
+    if input.range(of:"jan", options: [.regularExpression, .caseInsensitive]) != nil {
+      return "January"
+    }
+    else if input.range(of:"feb", options: [.regularExpression, .caseInsensitive]) != nil {
+      return "February"
+    }
+    else if input.range(of:"mar", options: [.regularExpression, .caseInsensitive]) != nil {
+      return "March"
+    }
+    else if input.range(of:"apr", options: [.regularExpression, .caseInsensitive]) != nil {
+      return "April"
+    }
+    else if input.range(of:"may", options: [.regularExpression, .caseInsensitive]) != nil {
+      return "May"
+    }
+    else if input.range(of:"jun", options: [.regularExpression, .caseInsensitive]) != nil  {
+      return "June"
+    }
+    else if input.range(of:"jul", options: [.regularExpression, .caseInsensitive]) != nil  {
+      return "July"
+    }
+    else if input.range(of:"aug", options: [.regularExpression, .caseInsensitive]) != nil  {
+      return "Asgust"
+    }
+    else if input.range(of:"sep", options: [.regularExpression, .caseInsensitive]) != nil {
+      return "September"
+    }
+    else if input.range(of:"oct", options: [.regularExpression, .caseInsensitive]) != nil {
+      return "October"
+    }
+    else if input.range(of:"nov", options: [.regularExpression, .caseInsensitive]) != nil {
+      return "November"
+    }
+    else if input.range(of:"dec", options: [.regularExpression, .caseInsensitive]) != nil  {
+      return "December"
+    }
+    else{
+      return ""
+    }
   }
 }
